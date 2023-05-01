4288f1ad89167d0dae8010622d4f78c401e60664
Eleonora907 <elenatroyan2010@ukr.net>
2023-05-01 20:43:51 +0200

    Edited

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,25 +3,34 @@ import '../css/03-feedback.css';
 import { loadFromLS, saveToLS } from './helpers';
 import throttle from 'lodash.throttle';
 
-// Select form elements and get initial form data from local storage
 const formElem = document.querySelector('.feedback-form');
-const nameElem = formElem.elements.name;
-const messageElem = formElem.elements.message;
-const formData = loadFromLS('feedbackFormData') ?? {};
-nameElem.value = formData?.name;
-messageElem.value = formData?.message;
+const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+formElem.elements.email.value = formData.email || '';
+formElem.elements.message.value = formData.message || '';
 
-// Save form data to local storage using a throttled function
-const saveFormToLS = () => {
-  const { name, message } = formData;
-  saveToLS('feedbackFormData', { name: nameElem.value, message: messageElem.value });
-};
-const throttledSaveFormToLS = throttle(saveFormToLS, 500);
+const saveFormData = throttle((key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+}, 500);
 
-// Handle form submission by saving form data to local storage and resetting the form
-formElem.addEventListener('submit', e => {
+formElem.addEventListener('input', () => {
+  const formData = {
+    email: formElem.elements.email.value,
+    message: formElem.elements.message.value,
+  };
+  saveFormData('feedback-form-state', formData);
+});
+
+formElem.addEventListener('submit', (e) => {
   e.preventDefault();
-  throttledSaveFormToLS();
+  const formData = {
+    email: formElem.elements.email.value,
+    message: formElem.elements.message.value,
+  };
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
   formElem.reset();
 });
+
+
+
 
